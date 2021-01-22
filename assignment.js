1b2e987d100887bc962fea4bec5a859ed5b6f466
Tazulislam <butterflybamfire12@gmail.com>
2021-01-22 23:15:57 +0600

    last touch in code

--- assignment.js
+++ assignment.js
@@ -1,38 +1,32 @@
 function kilometerToMeter(km){
     
     if(km < 0){
-        console.log("Distance can not be any negative value");
-        
+        console.log("Distance can not be any negative value");    
     }
     else{
-        var getkiloMeter = km;
         var convertToMeter = km * 1000;  //1 km = 1000m
     }
     return convertToMeter;
 }
-var result = kilometerToMeter(0);
-console.log(result);
+
 
 
 
 
 
 function budgetCalculator(watch, mobile, laptop){
-    var watchQuantity = watch;
+    
     var watchCost = watch * 50;
-
-    var mobileQuantity = mobile;
     var mobileCost = mobile * 100;
-
-    var laptopQuantity = laptop;
     var laptopCost = laptop * 500;
 
     var totalCost = watchCost + mobileCost + laptopCost;
 
     return totalCost;
 }
-var result = budgetCalculator(2, 0, 6);
-console.log(result);
+
+
+
 
 
 
@@ -52,17 +46,14 @@ else if(day <= 20){
     
 }
 else{
-    var firstTenDays = 10*100;
+    var firstTenDays = 10*100; 
     var secondTermCost = 10*80;
     var remainingDays = day - 20;
     var remainingDaysCost = remainingDays * 50;
-    billPayment = firstTenDays + secondTermCost + remainingDaysCost;
+    billPayment = firstTenDays + secondTermCost + remainingDaysCost; //secondTermCost refers 11th-20th days cost
 }
 return billPayment;
 }
-var result = hotelCost(11);
-console.log(result);
-
 
 
 
@@ -70,18 +61,19 @@ console.log(result);
 
 
 function megaFriend(arr){
-    var bigName = arr[0];
-    for(i = 0; i < arr.length; i++){
-       var element = arr[i];
-       if(element > arr[0] ){
-           bigName = element;
-       }
+    var maximumWord = 0;
+    var bigName;
+    
+    for(i=0; i < arr.length; i++){
+        if(arr[i].length > maximumWord){
+            var maximumWord = arr[i].length;
+            bigName = arr[i];
+        }
     }
     return bigName;
-    
 }
-var result = megaFriend(["akbar ali khan", "tamim iqbal", "jamal", "tazul islam nishad", "shekh mujibur rahman shaheb"]);
-console.log(result);
+
+
 
 
 
